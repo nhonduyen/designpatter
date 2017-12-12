ba54e35cdf5d44d446b4a6f232948f7ccd7cfa00
nhonduyen <nhonduyen@gmail.com>
2017-12-12 15:42:47 +0700

    + First Commnit

---
+++ FactoryPattern/ShapeFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class ShapeFactory
+    {
+        public Shape getShape(string shapeType)
+        {
+            if (string.IsNullOrEmpty(shapeType))
+                return null;
+            switch (shapeType.ToUpper())
+            {
+                case "RECTANGLE":
+                    return new Rectangle();
+                case "SQUARE":
+                    return new Square();
+                case "CIRCLE":
+                    return new Circle();
+            }
+            return null;
+        }
+    }
+}
---
+++ FactoryPattern/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Square:Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Draw square");
+        }
+    }
+}
