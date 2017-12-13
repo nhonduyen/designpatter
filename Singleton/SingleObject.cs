0fbe2e07f43c71b87911563f9eaeeaea422d2c0b
nhonduyen <nhonduyen@gmail.com>
2017-12-13 15:29:38 +0700

    + Builder + singleton

---
+++ Singleton/SingleObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class SingleObject
+    {
+        private static SingleObject instance = new SingleObject();
+
+        private SingleObject() { }
+
+        public static SingleObject getInstance()
+        {
+            return instance;
+        }
+
+        public void showMessage()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
